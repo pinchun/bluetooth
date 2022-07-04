33104156aa80dd598d84980e83506d7ec119d100
Carol <c0408892@gmail.com>
2022-07-04 16:38:22 +0800

    智慧屋藍芽版...因為容量問題只有部分功能

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Yes)
     flag += 1
     while (flag == 1) {
         blue = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
@@ -10,11 +10,44 @@ bluetooth.onBluetoothConnected(function () {
         } else if (blue == "b") {
             pins.digitalWritePin(DigitalPin.P16, 0)
         }
+        if (blue == "c") {
+            pins.servoWritePin(AnalogPin.P8, 0)
+        } else if (blue == "d") {
+            pins.servoWritePin(AnalogPin.P8, 180)
+        }
+        if (blue == "e") {
+            pins.servoWritePin(AnalogPin.P9, 0)
+        } else if (blue == "f") {
+            pins.servoWritePin(AnalogPin.P9, 100)
+        }
+        if (blue == "g") {
+            pins.analogWritePin(AnalogPin.P12, 500)
+            pins.analogWritePin(AnalogPin.P13, 1023)
+        } else if (blue == "h") {
+            pins.analogWritePin(AnalogPin.P12, 0)
+            pins.analogWritePin(AnalogPin.P13, 0)
+        }
+        if (blue == "1") {
+            basic.showIcon(IconNames.Happy)
+        } else if (blue == "2") {
+            basic.showIcon(IconNames.Sad)
+        } else if (blue == "3") {
+            basic.showIcon(IconNames.Asleep)
+        } else if (blue == "4") {
+            basic.showIcon(IconNames.Heart)
+        } else if (blue == "5") {
+            basic.showIcon(IconNames.Duck)
+        } else if (blue == "6") {
+            basic.showArrow(ArrowNames.North)
+        } else if (blue == "0") {
+            basic.showIcon(IconNames.House)
+        }
     }
 })
 bluetooth.onBluetoothDisconnected(function () {
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.No)
 })
 let blue = ""
 let flag = 0
 serial.redirectToUSB()
+basic.showIcon(IconNames.House)
